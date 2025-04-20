c55d036f7ac72b368c1159d6346fd499fd3ce866
Jeremy Watt <jermwatt@gmail.com>
2025-04-20 09:29:18 -0700

    vite repackage

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'src/popup/popup.html'),
+        content: resolve(__dirname, 'src/content/content.js'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+  },
+});
